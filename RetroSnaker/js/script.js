ebb3d81905ebd624419a9826011622598f782492
LWX <lwx9688@gmail.com>
2018-03-12 10:34:59 +0800

    update 18/03/12

--- RetroSnaker/js/script.js
+++ RetroSnaker/js/script.js
@@ -1,11 +1,11 @@
-var mapDiv = document.getElementById("map");
-var score = document.getElementById("board");
+var mapDiv;
+var score;
 
-var start = document.getElementsByClassName("start")[0];
-var pause = document.getElementsByClassName("pause")[0];
-var high = document.getElementsByClassName("high")[0];
-var medium = document.getElementsByClassName("medium")[0];
-var slow = document.getElementsByClassName("slow")[0];
+var start;
+var pause;
+var high;
+var medium;
+var slow;
 
 //the row and col of the grid
 var rows = 20;
@@ -16,13 +16,49 @@ var size = 20;
 
 //the container of grids
 var mapGrid = [];
-init();
+
+window.onload = function(){
+	init();
+}
 
 function init() {
+	mapDiv = document.getElementById("map");
+	score = document.getElementById("board")
+	start = document.getElementsByClassName("start")[0];
+	pause = document.getElementsByClassName("pause")[0];
+	high = document.getElementsByClassName("high")[0];
+	medium = document.getElementsByClassName("medium")[0];
+	slow = document.getElementsByClassName("slow")[0];
+	
 	drawMap(rows, cols);
 }
 
 //draw the map
 function drawMap(rowNum, colNum) {
+	//set the width and height of map
+	mapDiv.style.width = size * colNum + "px";
+	mapDiv.style.height = size * rowNum + "px";
 	
+	//draw grid line by line
+	for (var i = 0; i < rowNum; i++) {
+		//store the grids by array
+		var rowGrid = [];
+		
+		//create the element
+		var rowDiv = document.createElement("div");
+		
+		//add element to map
+		mapDiv.appendChild(rowDiv);
+
+		for (var j = 0; j < colNum; j++) {
+			//create the mini grid
+			var colDiv = document.createElement("div");
+			colDiv.className = "miniGrid";
+			
+			//add it in the div
+			rowDiv.appendChild(colDiv);
+			rowGrid.push(colDiv);
+		}
+		mapGrid.push(rowGrid);
+	}
 }
